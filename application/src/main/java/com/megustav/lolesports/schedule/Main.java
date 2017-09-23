c79795d761bf0f40ebe3c8eea4e079a7eadc7bac
MeGustav <krotaus@gmail.com>
2017-09-23 23:55:04 +0300

    Basic bot implementation

--- application/src/main/java/com/megustav/lolesports/schedule/Main.java
+++ application/src/main/java/com/megustav/lolesports/schedule/Main.java
@@ -1,15 +1,29 @@
 package com.megustav.lolesports.schedule;
 
+import com.megustav.lolesports.schedule.bot.Bot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 /**
  * Application entry point
  *
- * @author Megusav
+ * @author MeGusav
  *         23.09.17 20:38
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("[Test] Application started");
+        System.out.println("Starting application");
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ application/src/main/java/com/megustav/lolesports/schedule/bot/Bot.java
@@ -0,0 +1,58 @@
+package com.megustav.lolesports.schedule.bot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+/**
+ * A telegram bot providing information on the upcoming competitive League of Legends events
+ *
+ * @author MeGusav
+ *         23.09.17 23:37
+ */
+public class Bot extends TelegramLongPollingBot {
+
+    /** Bot Username */
+    private static final String BOT_NAME = "LolEsportsScheduleBot";
+    /**
+     * Bot token. Obviously to be moved to config.
+     * The value here is temporal for bot testing and is to be changed later.
+     */
+    private static final String BOT_TOKEN = "409557956:AAGeN4cSk9VBhtr3UpvJnCItKd1zLXb1eVk";
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (! update.hasMessage()) {
+            System.out.println("No message received");
+            return;
+        }
+        Message received = update.getMessage();
+        System.out.println("[====]");
+        System.out.println("Message received");
+        System.out.println("User: " + received.getFrom().getFirstName() + " " + received.getFrom().getLastName());
+        System.out.println("Content: " + received.getText());
+        System.out.println("[====]");
+        try {
+
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(received.getText().contains("Инна") ?
+                            "Люблю тебя. Дай еще супа" :
+                            "Not implemented yet");
+            sendMessage(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+}
