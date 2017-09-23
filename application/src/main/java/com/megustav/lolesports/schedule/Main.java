671d57ca48ea98e1ddd46e88051bdd97c4b317d4
MeGustav <krotaus@gmail.com>
2017-09-23 22:12:25 +0300

    Actual initial project structure

---
+++ application/src/main/java/com/megustav/lolesports/schedule/Main.java
@@ -0,0 +1,15 @@
+package com.megustav.lolesports.schedule;
+
+/**
+ * Application entry point
+ *
+ * @author Megusav
+ *         23.09.17 20:38
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("[Test] Application started");
+    }
+
+}
---
+++ dist/src/main/resources/bin/startup.sh
@@ -0,0 +1,1 @@
+java -cp ../lib/*:../conf/* com.megustav.lolesports.schedule.Main
